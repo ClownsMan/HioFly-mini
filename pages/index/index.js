999c437acc10a2a695b74a2f5527b254a693b55e
990015030 <785656687@qq.com>
2024-12-04 16:43:48 +0800

    修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,53 @@
 Page({
   data: {
-    swiperList: [
+    banners: [
       { id: 1, image: '/assets/images/swiper1.png' },
       { id: 2, image: '/assets/images/swiper2.png' },
-      { id: 3, image: '/assets/images/swiper3.png' }
     ],
-    quickTools: [
-      { id: 1, name: '车贷计算', icon: '/assets/icons/loan.png', url: '/pages/carLoan/carLoan' },
-      { id: 2, name: '充电桩', icon: '/assets/icons/charging.png', url: '/pages/chargingStation/chargingStation' },
-      { id: 3, name: '停车场', icon: '/assets/icons/parking.png', url: '/pages/parking/parking' },
-      { id: 4, name: '车型对比', icon: '/assets/icons/compare.png', url: '/pages/carCompare/carCompare' }
-    ],
-    carImages: [],
-    currentImageIndex: 0,
-    startX: 0,
-    moving: false
-  },
-
-  navigateToTool(e) {
-    const { url } = e.currentTarget.dataset;
-    wx.navigateTo({ url });
+    quickActions: [
+      { 
+        id: 1, 
+        name: '车贷计算', 
+        icon: '/assets/icons/loan.png',
+        url: '/pages/carLoan/carLoan'
+      },
+      { 
+        id: 2, 
+        name: '充电桩', 
+        icon: '/assets/icons/charging.png',
+        url: '/pages/chargingStation/chargingStation'
+      },
+      { 
+        id: 3, 
+        name: '停车场', 
+        icon: '/assets/icons/parking.png',
+        url: '/pages/parking/parking'
+      },
+      { 
+        id: 4, 
+        name: '车型对比', 
+        icon: '/assets/icons/compare.png',
+        url: '/pages/carCompare/carCompare'
+      }
+    ]
   },
 
   onShow() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
-        selected: 0  // 首页
+        selected: 0
       });
     }
   },
+
+  navigateTo(e) {
+    const { url } = e.currentTarget.dataset;
+    wx.navigateTo({ url });
+  },
+
+  changeCar() {
+    wx.navigateTo({
+      url: '/pages/bindCar/bindCar'
+    });
+  }
 }); 
